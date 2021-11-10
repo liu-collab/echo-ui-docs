390a6bb1c68207ce29441742f2a204130eedfdf6
liu-collab <1581133593@qq.com>
2021-11-10 10:20:00 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,53 @@
+const base = process.env.NODE_ENV === 'production' ? '/echo-ui-docs' : '';
+const { resolve } = require('path');
+
+module.exports = {
+  title: 'echo-ui-docs',
+  description: '',
+  // 扫描srcIncludes里面的 *.md文件
+  srcIncludes: ['src'],
+  alias: {
+    // 为了能在demo中正确的使用  import { X } from 'echo-ui-docs'
+    [`echo-ui-docs`]: resolve('./src'),
+  },
+  base,
+  themeConfig: {
+    // logo: '../logo.svg',
+    nav: [{ text: 'demo', link: '/math' }],
+    lang: 'zh-CN',
+    locales: {
+      '/': {
+        lang: 'zh-CN',
+        title: 'echo-ui-docs',
+        description: '',
+        label: '中文',
+        selectText: '语言',
+        nav: [{ text: '指南', link: '/' }],
+        sidebar: [
+          { text: '介绍', link: '/' },
+          { text: 'Button', link: '/components/button/' },
+        ],
+      },
+      '/en/': {
+        lang: 'en-US',
+        title: 'echo-ui-docs',
+        description: '',
+        label: 'English',
+        selectText: 'Languages',
+        nav: [{ text: 'Guide', link: '/' }],
+        sidebar: [
+          { text: 'Getting Started', link: '/en/' },
+          { text: 'Button', link: '/en/components/button/' },
+        ],
+      },
+    },
+    search: {
+      searchMaxSuggestions: 10,
+    },
+    repo: 'echo/echo-ui-docs',
+    repoLabel: 'Github',
+    lastUpdated: true,
+    prevLink: true,
+    nextLink: true,
+  },
+};
