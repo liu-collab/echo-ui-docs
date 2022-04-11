7fc48833e4afd38e656ad273a761ce61c50574c1
lyq <yuqi.liu@yuyidata.com>
2022-04-11 17:44:33 +0800

    delpoy

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  collectCoverageFrom: ['src/**/*.ts'],
-  moduleNameMapper: {},
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-  },
-  moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
-};
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import EButton from './button/button.vue';
-import EIcon from './icon/icon.vue';
-import EButtonGroup from './buttongroup/button-group.vue';
-import EAlert from './alert/alert.vue';
-import '../styles/button.scss';
-import '../styles/icon.scss';
-import '../styles/button-group.scss';
-import '../styles/alert.scss';
-export { EButton, EIcon, EButtonGroup, EAlert };
